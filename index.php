d86358317c45cd4ffc751642fbad014a7b1c2ffb
TwinType <willy@twintype.co>
2025-02-24 08:39:06 +0800

    改為php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TwinType</title>
 </head>
-<body style="display: flex;width: 100%;height: calc(100vh);padding: 0px;margin: 0px;align-items: center;justify-content: center;">
+<body style="display: flex;width: 100%;height: calc(96vh);padding: 0px;margin: 0px;align-items: center;justify-content: center;">
     <h1>Hello World</h1>
 </body>
 </html>
